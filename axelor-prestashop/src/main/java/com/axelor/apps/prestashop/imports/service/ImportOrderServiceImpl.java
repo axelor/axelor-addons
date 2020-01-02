9fcd0f4867ee6fd27bbf94a24dcca5eaf69540df
psh-axelor <p.shah@axelor.com>
2020-01-02 17:09:03 +0530

    Fix errors on build
    - Revert the changes made for Anomaly #23441
    (https://github.com/axelor/axelor-addons/commit/ef781363b4cad1c2cc90206b42c9d6a7abafd930)

--- axelor-prestashop/src/main/java/com/axelor/apps/prestashop/imports/service/ImportOrderServiceImpl.java
+++ axelor-prestashop/src/main/java/com/axelor/apps/prestashop/imports/service/ImportOrderServiceImpl.java
@@ -599,7 +599,8 @@ public class ImportOrderServiceImpl implements ImportOrderService {
         // Set all default information (product name, tax, unit, cost price)
         // Even if we'll override some of them later, it does not hurt to pass
         // through service method
-        saleOrderLineService.computeProductInformation(localLine, localOrder);
+        saleOrderLineService.computeProductInformation(
+            localLine, localOrder, localLine.getPackPriceSelect());
 
         localLine.setQty(BigDecimal.valueOf(remoteLine.getProductQuantity()));
         localLine.setProductName(remoteLine.getProductName());
@@ -701,7 +702,8 @@ public class ImportOrderServiceImpl implements ImportOrderService {
       // Set all default information (product name, tax, unit, cost price)
       // Even if we'll override some of them later, it does not hurt to pass
       // through service method
-      saleOrderLineService.computeProductInformation(localLine, localOrder);
+      saleOrderLineService.computeProductInformation(
+          localLine, localOrder, product.getPackPriceSelect());
 
       localLine.setQty(BigDecimal.ONE);
       localLine.setPrice(price);
