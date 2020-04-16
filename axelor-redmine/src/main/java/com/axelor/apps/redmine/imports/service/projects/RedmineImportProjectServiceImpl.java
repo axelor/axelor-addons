c43c13d0fe52763dbc116d0184d44826deacd6b4
Akshay Gandhi <a.gandhi@axelor.com>
2020-04-16 18:42:37 +0530

    RM#27414 Fixed compilation issues

--- axelor-redmine/src/main/java/com/axelor/apps/redmine/imports/service/projects/RedmineImportProjectServiceImpl.java
+++ axelor-redmine/src/main/java/com/axelor/apps/redmine/imports/service/projects/RedmineImportProjectServiceImpl.java
@@ -55,7 +55,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.function.Consumer;
 import java.util.stream.Collectors;
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
