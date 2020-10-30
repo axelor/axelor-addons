814184badc9e27aa16de9bb6ab1434cb48d23d46
psh-axelor <p.shah@axelor.com>
2020-10-30 10:26:34 +0530

    [Feature #21488] Hard coded mapping sync - update
    - Remove some changes

--- axelor-redmine/src/main/java/com/axelor/apps/redmine/imports/service/issues/RedmineImportTimeSpentServiceImpl.java
+++ axelor-redmine/src/main/java/com/axelor/apps/redmine/imports/service/issues/RedmineImportTimeSpentServiceImpl.java
@@ -402,10 +402,6 @@ public class RedmineImportTimeSpentServiceImpl extends RedmineImportService
     timesheetLine.setActivityTypeSelect(
         activityType != null && !activityType.isEmpty() ? selectionMap.get(activityType) : null);
 
-    if (teamTask != null && !timesheetLine.getToInvoice()) {
-      timesheetLine.setToInvoice(teamTask.getToInvoice());
-    }
-
     Timesheet timesheet =
         timesheetRepo
             .all()
