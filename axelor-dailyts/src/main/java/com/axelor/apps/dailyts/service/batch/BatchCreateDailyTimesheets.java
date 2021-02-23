002ab9f8d3f22b723b9ab2eac1f8c426969160d2
psh-axelor <p.shah@axelor.com>
2021-02-23 17:42:07 +0530

    [Feature #34952] DAILY TIMESHEET : scheduler
    - Check work day, leave day and public holiday before creating dailytimesheet from batch

--- axelor-dailyts/src/main/java/com/axelor/apps/dailyts/service/batch/BatchCreateDailyTimesheets.java
+++ axelor-dailyts/src/main/java/com/axelor/apps/dailyts/service/batch/BatchCreateDailyTimesheets.java
@@ -17,23 +17,36 @@
  */
 package com.axelor.apps.dailyts.service.batch;
 
+import com.axelor.apps.base.service.weeklyplanning.WeeklyPlanningService;
 import com.axelor.apps.dailyts.service.timesheet.DailyTimesheetService;
 import com.axelor.apps.hr.db.DailyTimesheet;
+import com.axelor.apps.hr.db.Employee;
 import com.axelor.apps.hr.db.HrBatch;
+import com.axelor.apps.hr.db.LeaveRequest;
 import com.axelor.apps.hr.db.repo.DailyTimesheetRepository;
 import com.axelor.apps.hr.service.batch.BatchStrategy;
+import com.axelor.apps.hr.service.leave.LeaveService;
+import com.axelor.apps.hr.service.publicHoliday.PublicHolidayHrService;
 import com.axelor.auth.db.User;
 import com.axelor.auth.db.repo.UserRepository;
+import com.axelor.exception.AxelorException;
+import com.axelor.exception.service.TraceBackService;
 import com.google.inject.Inject;
 import com.google.inject.persist.Transactional;
+import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 import java.util.Set;
+import org.apache.commons.collections.CollectionUtils;
 
 public class BatchCreateDailyTimesheets extends BatchStrategy {
 
-  @Inject UserRepository userRepository;
-  @Inject DailyTimesheetRepository dailyTimesheetRepository;
-  @Inject DailyTimesheetService dailyTimesheetService;
+  @Inject protected UserRepository userRepository;
+  @Inject protected DailyTimesheetRepository dailyTimesheetRepository;
+  @Inject protected DailyTimesheetService dailyTimesheetService;
+  @Inject protected WeeklyPlanningService weeklyPlanningService;
+  @Inject protected LeaveService leaveService;
+  @Inject protected PublicHolidayHrService publicHolidayHrService;
 
   @Override
   @Transactional
@@ -48,23 +61,57 @@ public class BatchCreateDailyTimesheets extends BatchStrategy {
     }
 
     for (User dailyTsUser : dailyTsUserSet) {
-      long count =
-          dailyTimesheetRepository
-              .all()
-              .filter(
-                  "self.dailyTimesheetUser = ?1 and self.dailyTimesheetDate = ?2",
-                  dailyTsUser,
-                  dailyTsDate)
-              .count();
 
-      if (count == 0) {
-        DailyTimesheet dailyTimesheet = new DailyTimesheet();
-        dailyTimesheet.setDailyTimesheetDate(dailyTsDate);
-        dailyTimesheet.setDailyTimesheetUser(dailyTsUser);
-        dailyTimesheetService.updateFromTimesheetAndFavs(dailyTimesheet);
-        dailyTimesheetRepository.save(dailyTimesheet);
-        incrementDone();
+      try {
+        long count =
+            dailyTimesheetRepository
+                .all()
+                .filter(
+                    "self.dailyTimesheetUser = ?1 and self.dailyTimesheetDate = ?2",
+                    dailyTsUser,
+                    dailyTsDate)
+                .count();
+
+        Employee employee = dailyTsUser.getEmployee();
+
+        if (count == 0
+            && employee != null
+            && employee.getWeeklyPlanning() != null
+            && weeklyPlanningService.getWorkingDayValueInDays(
+                    employee.getWeeklyPlanning(), dailyTsDate)
+                > 0
+            && !isFullDayLeave(dailyTsUser, dailyTsDate)
+            && !publicHolidayHrService.checkPublicHolidayDay(
+                dailyTsDate, employee.getPublicHolidayEventsPlanning())) {
+          DailyTimesheet dailyTimesheet = new DailyTimesheet();
+          dailyTimesheet.setDailyTimesheetDate(dailyTsDate);
+          dailyTimesheet.setDailyTimesheetUser(dailyTsUser);
+          dailyTimesheetService.updateFromTimesheetAndFavs(dailyTimesheet);
+          dailyTimesheetRepository.save(dailyTimesheet);
+          incrementDone();
+        }
+      } catch (AxelorException e) {
+        incrementAnomaly();
+        TraceBackService.trace(e, "", batch.getId());
       }
     }
+  }
+
+  protected boolean isFullDayLeave(User dailyTsUser, LocalDate dailyTsDate) throws AxelorException {
+
+    List<LeaveRequest> leaveRequestList = leaveService.getLeaves(dailyTsUser, dailyTsDate);
+
+    if (CollectionUtils.isNotEmpty(leaveRequestList)) {
+
+      for (LeaveRequest leave : leaveRequestList) {
+
+        if (leaveService.computeDuration(leave, dailyTsDate, dailyTsDate).compareTo(BigDecimal.ONE)
+            == 0) {
+          return true;
+        }
+      }
+    }
+
+    return false;
   }
 }
