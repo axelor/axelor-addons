0fb7d88c072c82465162a7c9bde1a148780f4ed8
rch-axelor <r.chauhan@axelor.com>
2023-04-28 17:14:14 +0530

    RM#63466 & RM#60890 EShop : zip control location & outOfStock order

---
+++ axelor-customer-portal/src/main/webapp/eshop/precache-manifest.8ff05c64df16f08cf927393e505be36f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce03ca7d17a8c61d75612a770c8fdb95",
+    "url": "./index.html"
+  },
+  {
+    "revision": "0850226a144d4e1641c5",
+    "url": "./static/css/2.ea2b5425.chunk.css"
+  },
+  {
+    "revision": "153891b0d90821259808",
+    "url": "./static/css/main.dab30c4e.chunk.css"
+  },
+  {
+    "revision": "0850226a144d4e1641c5",
+    "url": "./static/js/2.61cced72.chunk.js"
+  },
+  {
+    "revision": "6f80c71daa79363a8cb4e03a3ff244ea",
+    "url": "./static/js/2.61cced72.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "153891b0d90821259808",
+    "url": "./static/js/main.29136a02.chunk.js"
+  },
+  {
+    "revision": "4fd406c2a399565eacf7",
+    "url": "./static/js/runtime-main.c86b7950.js"
+  },
+  {
+    "revision": "013f0b90a4cae7b8bbf13e3fd9e7dc4a",
+    "url": "./static/media/getFetch.013f0b90.cjs"
+  }
+]);
