2af30e4658da696046df198e4387c21f10894bf6
rch-axelor <r.chauhan@axelor.com>
2023-06-05 18:49:09 +0530

    RM#64099 & RM#64101 EShop : address computation & city search

--- axelor-customer-portal/src/main/java/com/axelor/apps/customer/portal/web/service/CityWebService.java
+++ axelor-customer-portal/src/main/java/com/axelor/apps/customer/portal/web/service/CityWebService.java
@@ -17,18 +17,22 @@
  */
 package com.axelor.apps.customer.portal.web.service;
 
+import com.axelor.apps.base.AxelorException;
 import com.axelor.apps.base.db.City;
 import com.axelor.apps.customer.portal.service.response.PortalRestResponse;
 import com.axelor.apps.customer.portal.service.response.ResponseGeneratorFactory;
 import com.axelor.apps.customer.portal.service.response.generator.ResponseGenerator;
+import com.axelor.common.StringUtils;
 import com.axelor.db.JpaSecurity;
 import com.axelor.db.JpaSecurity.AccessType;
 import com.axelor.inject.Beans;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
@@ -41,9 +45,17 @@ public class CityWebService extends AbstractWebService {
   public PortalRestResponse fetch(
       @QueryParam("sort") String sort,
       @QueryParam("page") int page,
-      @QueryParam("limit") int limit) {
+      @QueryParam("limit") int limit,
+      @QueryParam("searchInput") String searchInput) {
 
-    List<City> cities = fetch(City.class, null, null, sort, limit, page);
+    final Map<String, Object> params = new HashMap<String, Object>();
+    StringBuilder filter = new StringBuilder();
+    if (StringUtils.notBlank(searchInput)) {
+      filter.append("(lower(self.name) like :pattern)");
+      params.put("pattern", String.format("%%%s%%", searchInput.toLowerCase()));
+    }
+
+    List<City> cities = fetch(City.class, filter.toString(), params, sort, limit, page);
     Beans.get(JpaSecurity.class)
         .check(AccessType.READ, City.class, cities.stream().map(City::getId).toArray(Long[]::new));
 
@@ -55,6 +67,24 @@ public class CityWebService extends AbstractWebService {
     PortalRestResponse response = new PortalRestResponse();
     response.setTotal(totalQuery(City.class, null, null));
     response.setOffset((page - 1) * limit);
+    return response.setData(data).success();
+  }
+
+  @GET
+  @Produces(MediaType.APPLICATION_JSON)
+  @Path("/{id}")
+  public PortalRestResponse fetch(@PathParam("id") Long id) throws AxelorException {
+
+    final Map<String, Object> params = new HashMap<String, Object>();
+    final String filter = "self.id = :id";
+    params.put("id", id);
+
+    List<City> cities = fetch(City.class, filter, params, null, 0, 1);
+    City city = cities.get(0);
+    Beans.get(JpaSecurity.class).check(AccessType.READ, City.class, city.getId());
+
+    Map<String, Object> data = ResponseGeneratorFactory.of(City.class.getName()).generate(city);
+    PortalRestResponse response = new PortalRestResponse();
     return response.setData(data).success();
   }
 }
